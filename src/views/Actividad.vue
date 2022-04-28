faf78f6dc998f22e5a1a275b9ac225468634893f
AndresDiazPinto <andresvis565@gmail.com>
2022-04-28 09:17:08 -0500

    Descargable
    Se vincula AD y se genera descagable componente OK

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -3,7 +3,23 @@
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.h-100.d-flex.align-items-center
+          figure
+            img(src="@/assets/template/f-v.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
+          h2 Aprendiendo y comprendiendo 
+        p.mb-4 Identificar a nivel general la apropiación del conocimiento en el componente formativo revisado.
+        .tarjeta.bgr3.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Falso o verdadero.
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/AD1/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
 
 </template>
 
